01f0883f00185e805887fdde01861ab2150db009
saggu <amandeep.s.saggu@gmail.com>
2014-10-14 21:37:31 -0700

    added api support for ads

--- java/dig-web-services/src/main/java/edu/isi/dig/webservices/DereferenceURIServlet.java
+++ java/dig-web-services/src/main/java/edu/isi/dig/webservices/DereferenceURIServlet.java
@@ -30,9 +30,13 @@ public class DereferenceURIServlet {
 	
 	final String INDEX_IMAGES = "images";
 	final String INDEX_TYPE_IMAGE = "image";
+	final String INDEX_PAGES = "pages";
+	final String INDEX_TYPE_PAGE = "page";
 	final String SEARCH_RESULTS="results";
 	final String CLUSTER_NAME = "cluster.name";
 	final String CLUSTER_NAME_VALUE = "dig_isi";
+	final String ELASTICSEARCH_HOST = "karma-dig-service.cloudapp.net"; 
+	final int ELASTICSEARCH_PORT = 55309;
 	
 	Client esClient=null;
 	TransportClient ts =null;
@@ -43,18 +47,61 @@ public class DereferenceURIServlet {
 	
 	@GET
 	@Path("/images/{sha}/{epoch}/processed")
-	public String GetImagesURLs(@PathParam("sha") String sha, @PathParam("epoch") String epoch){
+	public String GetElasticSearchImages(@PathParam("sha") String sha, @PathParam("epoch") String epoch){
+		
+				return GetImagesURLs(sha, epoch, INDEX_IMAGES, INDEX_TYPE_IMAGE);
+	}
+	
+	@GET
+	@Path("/images/{sha}/latest/processed")
+	public String GetElasticSearchImagesBySha(@PathParam("sha") String sha){
+			
+			return GetImageURLsBySha(sha, INDEX_IMAGES, INDEX_TYPE_IMAGE);
+		}
+	
+	
+	@GET
+	@Path("/images/{sha}/processed")
+	public String GetElasticSearchImageAllEpochs(@PathParam("sha") String sha){
+		
+		return GetImagesAllEpochs(sha, INDEX_IMAGES, INDEX_TYPE_IMAGE);
+	}
+	
+	@GET
+	@Path("/pages/{sha}/{epoch}/processed")
+	public String GetElasticSearchPages(@PathParam("sha") String sha, @PathParam("epoch") String epoch){
+		
+				return GetPagesURLs(sha, epoch, INDEX_PAGES, INDEX_TYPE_PAGE);
+	}
+	
+	@GET
+	@Path("/pages/{sha}/latest/processed")
+	public String GetElasticSearchPagesBySha(@PathParam("sha") String sha){
+			
+			return GetPageURLsBySha(sha, INDEX_PAGES, INDEX_TYPE_PAGE);
+		}
+	
+	
+	@GET
+	@Path("/pages/{sha}/processed")
+	public String GetElasticSearchAllEpochs(@PathParam("sha") String sha){
+		
+		return GetPagesAllEpochs(sha, INDEX_PAGES, INDEX_TYPE_PAGE);
+	}
+	
+	
+	public String GetImagesURLs(String sha, String epoch,String indexName, String indexType){
 		
 		try{
 			settings = ImmutableSettings.settingsBuilder()
 					 					.put(CLUSTER_NAME, CLUSTER_NAME_VALUE).build();
 			ts = new TransportClient(settings);
-			esClient = ts.addTransportAddress(new InetSocketTransportAddress("karma-dig-service.cloudapp.net", 55309));
+			esClient = ts.addTransportAddress(new InetSocketTransportAddress(ELASTICSEARCH_HOST, ELASTICSEARCH_PORT));
 			
 			SearchRequestBuilder srbSha = esClient.prepareSearch()
 				  .setQuery(QueryBuilders.matchQuery(SearchFieldsES.SHA1, sha))
-				  .setIndices(INDEX_IMAGES)
-				  .setTypes(INDEX_TYPE_IMAGE)
+				  .setIndices(indexName)
+				  .setTypes(indexType)
 				  .addField(SearchFieldsES.NATIVE_URL)
 				  .addField(SearchFieldsES.CACHE_URL)
 				  .addField(SearchFieldsES.CONTENT_URL)
@@ -124,104 +171,100 @@ public class DereferenceURIServlet {
 		}
 				
 	}
-	
-	@GET
-	@Path("/images/{sha}/latest/processed")
-	public String GetImageURLsBySha(@PathParam("sha") String sha){
-		try{
+
+
+	public String GetImageURLsBySha(String sha, String indexName, String indexType){
+	try{
+			
+			if(sha.trim() != "")
+			{
+				settings = ImmutableSettings.settingsBuilder()
+	 										.put(CLUSTER_NAME, CLUSTER_NAME_VALUE).build();
+				ts = new TransportClient(settings);
+				esClient = ts.addTransportAddress(new InetSocketTransportAddress(ELASTICSEARCH_HOST, ELASTICSEARCH_PORT));
 				
-				if(sha.trim() != "")
-				{
-					settings = ImmutableSettings.settingsBuilder()
-		 										.put(CLUSTER_NAME, CLUSTER_NAME_VALUE).build();
-					ts = new TransportClient(settings);
-					esClient = ts.addTransportAddress(new InetSocketTransportAddress("karma-dig-service.cloudapp.net", 55309));
-					
-					SearchRequestBuilder srbSha = esClient.prepareSearch()
-														  .setQuery(QueryBuilders.matchQuery(SearchFieldsES.SHA1, sha))
-														  .setIndices(INDEX_IMAGES)
-														  .setTypes(INDEX_TYPE_IMAGE)
-														  .addField(SearchFieldsES.NATIVE_URL)
-														  .addField(SearchFieldsES.CACHE_URL)
-														  .addField(SearchFieldsES.CONTENT_URL)
-														  .addField(SearchFieldsES.MEMEX_URL)
-														  .addField(SearchFieldsES.CONTENT_SHA1)
-														  .addField(SearchFieldsES.EPOCH)
-														  .addField(SearchFieldsES.SHA1)
-														  .addField(SearchFieldsES.SOURCE)
-														  .addSort(SearchFieldsES.EPOCH, SortOrder.DESC);
-									
-					
-					multiResp = esClient.prepareMultiSearch()
-										.add(srbSha)
-										.execute()
-										.actionGet();
-					
-					
-					ts.close();
-					
-				}
+				SearchRequestBuilder srbSha = esClient.prepareSearch()
+													  .setQuery(QueryBuilders.matchQuery(SearchFieldsES.SHA1, sha))
+													  .setIndices(indexName)
+													  .setTypes(indexType)
+													  .addField(SearchFieldsES.NATIVE_URL)
+													  .addField(SearchFieldsES.CACHE_URL)
+													  .addField(SearchFieldsES.CONTENT_URL)
+													  .addField(SearchFieldsES.MEMEX_URL)
+													  .addField(SearchFieldsES.CONTENT_SHA1)
+													  .addField(SearchFieldsES.EPOCH)
+													  .addField(SearchFieldsES.SHA1)
+													  .addField(SearchFieldsES.SOURCE)
+													  .addSort(SearchFieldsES.EPOCH, SortOrder.DESC);
+								
 				
-				 MultiSearchResponse.Item item = multiResp.getResponses()[0]; //Can't figure out why would it have more Items than one 
+				multiResp = esClient.prepareMultiSearch()
+									.add(srbSha)
+									.execute()
+									.actionGet();
 				
-				SearchResponse searchResp = item.getResponse();
-				SearchHit[] searchHit = searchResp.getHits().getHits();
 				
-				JSONObject parentObj= new JSONObject();
-				JSONArray jArray = new JSONArray();
-				JSONObject obj = new JSONObject();
-				
-				if(searchHit.length > 1){
-				
-					SearchHit searchHitLatest = searchHit[0]; //we need the latest, get the first one, sorted by epoch in descending order
-					Map<String,SearchHitField> map = searchHitLatest.getFields();
-					obj.accumulate(map.get(SearchFieldsES.NATIVE_URL).getName(), map.get(SearchFieldsES.NATIVE_URL).getValue());
-					obj.accumulate(map.get(SearchFieldsES.CONTENT_URL).getName(), map.get(SearchFieldsES.CONTENT_URL).getValue());
-					obj.accumulate(map.get(SearchFieldsES.CACHE_URL).getName(), map.get(SearchFieldsES.CACHE_URL).getValue());
-					obj.accumulate(map.get(SearchFieldsES.MEMEX_URL).getName(), map.get(SearchFieldsES.MEMEX_URL).getValue());
-					obj.accumulate(map.get(SearchFieldsES.SHA1).getName(), map.get(SearchFieldsES.SHA1).getValue());
-					obj.accumulate(map.get(SearchFieldsES.SOURCE).getName(), map.get(SearchFieldsES.SOURCE).getValue());
-					obj.accumulate(map.get(SearchFieldsES.CONTENT_SHA1).getName(), map.get(SearchFieldsES.CONTENT_SHA1).getValue());
-					obj.accumulate(map.get(SearchFieldsES.EPOCH).getName(), map.get(SearchFieldsES.EPOCH).getValue());
-				}
-				
-				jArray.add(obj);
-				
-				parentObj.accumulate(SEARCH_RESULTS, jArray);
-				
-				return parentObj.toString();
-		
-			}
-			catch(Exception e){
-				return e.toString();
-			}
-			finally{
-				
-				if(ts!=null)
-					ts.close();
-				
-				if(esClient!=null)
-					esClient.close();
+				ts.close();
 				
 			}
 			
+			 MultiSearchResponse.Item item = multiResp.getResponses()[0]; //Can't figure out why would it have more Items than one 
+			
+			SearchResponse searchResp = item.getResponse();
+			SearchHit[] searchHit = searchResp.getHits().getHits();
+			
+			JSONObject parentObj= new JSONObject();
+			JSONArray jArray = new JSONArray();
+			JSONObject obj = new JSONObject();
+			
+			if(searchHit.length > 1){
+			
+				SearchHit searchHitLatest = searchHit[0]; //we need the latest, get the first one, sorted by epoch in descending order
+				Map<String,SearchHitField> map = searchHitLatest.getFields();
+				obj.accumulate(map.get(SearchFieldsES.NATIVE_URL).getName(), map.get(SearchFieldsES.NATIVE_URL).getValue());
+				obj.accumulate(map.get(SearchFieldsES.CONTENT_URL).getName(), map.get(SearchFieldsES.CONTENT_URL).getValue());
+				obj.accumulate(map.get(SearchFieldsES.CACHE_URL).getName(), map.get(SearchFieldsES.CACHE_URL).getValue());
+				obj.accumulate(map.get(SearchFieldsES.MEMEX_URL).getName(), map.get(SearchFieldsES.MEMEX_URL).getValue());
+				obj.accumulate(map.get(SearchFieldsES.SHA1).getName(), map.get(SearchFieldsES.SHA1).getValue());
+				obj.accumulate(map.get(SearchFieldsES.SOURCE).getName(), map.get(SearchFieldsES.SOURCE).getValue());
+				obj.accumulate(map.get(SearchFieldsES.CONTENT_SHA1).getName(), map.get(SearchFieldsES.CONTENT_SHA1).getValue());
+				obj.accumulate(map.get(SearchFieldsES.EPOCH).getName(), map.get(SearchFieldsES.EPOCH).getValue());
+			}
+			
+			jArray.add(obj);
+			
+			parentObj.accumulate(SEARCH_RESULTS, jArray);
+			
+			return parentObj.toString();
+	
 		}
+		catch(Exception e){
+			return e.toString();
+		}
+		finally{
+			
+			if(ts!=null)
+				ts.close();
+			
+			if(esClient!=null)
+				esClient.close();
+			
+		}
+		
+	}
 	
-	
-	@GET
-	@Path("/images/{sha}/processed")
-	public String GetImagesAllEpochs(@PathParam("sha") String sha){
+public String GetImagesAllEpochs(String sha, String indexName, String indexType){
 		
 		try{
 			settings = ImmutableSettings.settingsBuilder()
 					 					.put(CLUSTER_NAME, CLUSTER_NAME_VALUE).build();
 			ts = new TransportClient(settings);
-			esClient = ts.addTransportAddress(new InetSocketTransportAddress("karma-dig-service.cloudapp.net", 55309));
+			esClient = ts.addTransportAddress(new InetSocketTransportAddress(ELASTICSEARCH_HOST, ELASTICSEARCH_PORT));
 			
 			SearchRequestBuilder srbSha = esClient.prepareSearch()
 				  .setQuery(QueryBuilders.matchQuery(SearchFieldsES.SHA1, sha))
-				  .setIndices(INDEX_IMAGES)
-				  .setTypes(INDEX_TYPE_IMAGE)
+				  .setIndices(indexName)
+				  .setTypes(indexType)
 				  .addField(SearchFieldsES.NATIVE_URL)
 				  .addField(SearchFieldsES.CACHE_URL)
 				  .addField(SearchFieldsES.CONTENT_URL)
@@ -279,5 +322,224 @@ public class DereferenceURIServlet {
 		}
 				
 	}
+
+public String GetPagesURLs(String sha, String epoch,String indexName, String indexType){
+	
+	try{
+		settings = ImmutableSettings.settingsBuilder()
+				 					.put(CLUSTER_NAME, CLUSTER_NAME_VALUE).build();
+		ts = new TransportClient(settings);
+		esClient = ts.addTransportAddress(new InetSocketTransportAddress(ELASTICSEARCH_HOST, ELASTICSEARCH_PORT));
+		
+		SearchRequestBuilder srbSha = esClient.prepareSearch()
+			  .setQuery(QueryBuilders.matchQuery(SearchFieldsES.SHA1, sha))
+			  .setIndices(indexName)
+			  .setTypes(indexType)
+			  .addField(SearchFieldsES.NATIVE_URL)
+			  .addField(SearchFieldsES.CACHE_URL)
+			  .addField(SearchFieldsES.MEMEX_URL)
+			  .addField(SearchFieldsES.EPOCH)
+			  .addField(SearchFieldsES.SHA1)
+			  .addField(SearchFieldsES.SOURCE);
+		SearchRequestBuilder srbEpoch = esClient.prepareSearch()
+				.setQuery(QueryBuilders.matchQuery(SearchFieldsES.EPOCH, epoch))
+				.setIndices(INDEX_IMAGES)
+		  		.setTypes(INDEX_IMAGES)
+		  		.addField(SearchFieldsES.NATIVE_URL)
+		  		.addField(SearchFieldsES.CACHE_URL)
+		  		.addField(SearchFieldsES.MEMEX_URL)
+		  		.addField(SearchFieldsES.EPOCH)
+		  		.addField(SearchFieldsES.SHA1)
+		  		.addField(SearchFieldsES.SOURCE);
+		
+		
+		multiResp = esClient.prepareMultiSearch()
+							.add(srbSha)
+							.add(srbEpoch)
+							.execute()
+							.actionGet();
+		
+		MultiSearchResponse.Item item = multiResp.getResponses()[0];	
+		
+		SearchHit[] searchHit;
+		
+		Map<String,SearchHitField> map ;
+		SearchResponse searchResp = item.getResponse();
+		searchHit = searchResp.getHits().getHits();
+		JSONObject parentObj= new JSONObject();
+		JSONArray jArray = new JSONArray();
+
+		if(searchHit.length > 1) {
+			for(SearchHit sr : searchHit){
+					map = sr.getFields();
+					JSONObject obj = new JSONObject();
+					
+					obj.accumulate(map.get(SearchFieldsES.NATIVE_URL).getName(), map.get(SearchFieldsES.NATIVE_URL).getValue());
+					obj.accumulate(map.get(SearchFieldsES.CACHE_URL).getName(), map.get(SearchFieldsES.CACHE_URL).getValue());
+					obj.accumulate(map.get(SearchFieldsES.MEMEX_URL).getName(), map.get(SearchFieldsES.MEMEX_URL).getValue());
+					obj.accumulate(map.get(SearchFieldsES.SHA1).getName(), map.get(SearchFieldsES.SHA1).getValue());
+					obj.accumulate(map.get(SearchFieldsES.SOURCE).getName(), map.get(SearchFieldsES.SOURCE).getValue());
+					obj.accumulate(map.get(SearchFieldsES.EPOCH).getName(), map.get(SearchFieldsES.EPOCH).getValue());
+					
+					jArray.add(obj);
+			}
+		}
+		
+		parentObj.accumulate(SEARCH_RESULTS, jArray);
+		return parentObj.toString();
+	}catch(Exception e){
+		return e.toString();
+	}finally{
+		
+		if(ts!=null)
+			ts.close();
+		if(esClient!=null)
+			esClient.close();
+	}
+			
+}
+
+
+public String GetPageURLsBySha(String sha, String indexName, String indexType){
+try{
+		
+		if(sha.trim() != "")
+		{
+			settings = ImmutableSettings.settingsBuilder()
+ 										.put(CLUSTER_NAME, CLUSTER_NAME_VALUE).build();
+			ts = new TransportClient(settings);
+			esClient = ts.addTransportAddress(new InetSocketTransportAddress(ELASTICSEARCH_HOST, ELASTICSEARCH_PORT));
+			
+			SearchRequestBuilder srbSha = esClient.prepareSearch()
+												  .setQuery(QueryBuilders.matchQuery(SearchFieldsES.SHA1, sha))
+												  .setIndices(indexName)
+												  .setTypes(indexType)
+												  .addField(SearchFieldsES.NATIVE_URL)
+												  .addField(SearchFieldsES.CACHE_URL)
+												  .addField(SearchFieldsES.MEMEX_URL)
+												  .addField(SearchFieldsES.EPOCH)
+												  .addField(SearchFieldsES.SHA1)
+												  .addField(SearchFieldsES.SOURCE)
+												  .addSort(SearchFieldsES.EPOCH, SortOrder.DESC);
+							
+			
+			multiResp = esClient.prepareMultiSearch()
+								.add(srbSha)
+								.execute()
+								.actionGet();
+			
+			
+			ts.close();
+			
+		}
+		
+		 MultiSearchResponse.Item item = multiResp.getResponses()[0]; //Can't figure out why would it have more Items than one 
+		
+		SearchResponse searchResp = item.getResponse();
+		SearchHit[] searchHit = searchResp.getHits().getHits();
+		
+		JSONObject parentObj= new JSONObject();
+		JSONArray jArray = new JSONArray();
+		JSONObject obj = new JSONObject();
+		
+		if(searchHit.length > 1){
+		
+			SearchHit searchHitLatest = searchHit[0]; //we need the latest, get the first one, sorted by epoch in descending order
+			Map<String,SearchHitField> map = searchHitLatest.getFields();
+			obj.accumulate(map.get(SearchFieldsES.NATIVE_URL).getName(), map.get(SearchFieldsES.NATIVE_URL).getValue());
+			obj.accumulate(map.get(SearchFieldsES.CACHE_URL).getName(), map.get(SearchFieldsES.CACHE_URL).getValue());
+			obj.accumulate(map.get(SearchFieldsES.MEMEX_URL).getName(), map.get(SearchFieldsES.MEMEX_URL).getValue());
+			obj.accumulate(map.get(SearchFieldsES.SHA1).getName(), map.get(SearchFieldsES.SHA1).getValue());
+			obj.accumulate(map.get(SearchFieldsES.SOURCE).getName(), map.get(SearchFieldsES.SOURCE).getValue());
+			obj.accumulate(map.get(SearchFieldsES.EPOCH).getName(), map.get(SearchFieldsES.EPOCH).getValue());
+		}
+		
+		jArray.add(obj);
+		
+		parentObj.accumulate(SEARCH_RESULTS, jArray);
+		
+		return parentObj.toString();
+
+	}
+	catch(Exception e){
+		return e.toString();
+	}
+	finally{
+		
+		if(ts!=null)
+			ts.close();
+		
+		if(esClient!=null)
+			esClient.close();
+		
+	}
+	
+}
+
+public String GetPagesAllEpochs(String sha, String indexName, String indexType){
+	
+	try{
+		settings = ImmutableSettings.settingsBuilder()
+				 					.put(CLUSTER_NAME, CLUSTER_NAME_VALUE).build();
+		ts = new TransportClient(settings);
+		esClient = ts.addTransportAddress(new InetSocketTransportAddress(ELASTICSEARCH_HOST, ELASTICSEARCH_PORT));
+		
+		SearchRequestBuilder srbSha = esClient.prepareSearch()
+			  .setQuery(QueryBuilders.matchQuery(SearchFieldsES.SHA1, sha))
+			  .setIndices(indexName)
+			  .setTypes(indexType)
+			  .addField(SearchFieldsES.NATIVE_URL)
+			  .addField(SearchFieldsES.CACHE_URL)
+			  .addField(SearchFieldsES.MEMEX_URL)
+			  .addField(SearchFieldsES.EPOCH)
+			  .addField(SearchFieldsES.SHA1)
+			  .addField(SearchFieldsES.SOURCE)
+			  .addSort(SearchFieldsES.EPOCH, SortOrder.DESC);
+		
+		multiResp = esClient.prepareMultiSearch()
+							.add(srbSha)
+							.execute()
+							.actionGet();
+		
+		MultiSearchResponse.Item item = multiResp.getResponses()[0];	//should be the first one, no point of getting more than responses.
+																		// will check the ElasticCode later to understand it better
+		
+		SearchHit[] searchHit;
+		
+		Map<String,SearchHitField> map ;
+		SearchResponse searchResp = item.getResponse();
+		searchHit = searchResp.getHits().getHits();
+		JSONObject parentObj= new JSONObject();
+		JSONArray jArray = new JSONArray();
+
+		if(searchHit.length > 1) {
+			for(SearchHit sr : searchHit){
+					map = sr.getFields();
+					JSONObject obj = new JSONObject();
+					
+					obj.accumulate(map.get(SearchFieldsES.NATIVE_URL).getName(), map.get(SearchFieldsES.NATIVE_URL).getValue());
+					obj.accumulate(map.get(SearchFieldsES.CACHE_URL).getName(), map.get(SearchFieldsES.CACHE_URL).getValue());
+					obj.accumulate(map.get(SearchFieldsES.MEMEX_URL).getName(), map.get(SearchFieldsES.MEMEX_URL).getValue());
+					obj.accumulate(map.get(SearchFieldsES.SHA1).getName(), map.get(SearchFieldsES.SHA1).getValue());
+					obj.accumulate(map.get(SearchFieldsES.SOURCE).getName(), map.get(SearchFieldsES.SOURCE).getValue());
+					obj.accumulate(map.get(SearchFieldsES.EPOCH).getName(), map.get(SearchFieldsES.EPOCH).getValue());
+					
+					jArray.add(obj);
+			}
+		}
+		
+		parentObj.accumulate(SEARCH_RESULTS, jArray);
+		return parentObj.toString();
+	}catch(Exception e){
+		return e.toString();
+	}finally{
+		
+		if(ts!=null)
+			ts.close();
+		if(esClient!=null)
+			esClient.close();
+	}
+			
+}
 		
 }
